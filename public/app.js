341c89db9133a2c84b1956bad2b345d8810d25e4
tarikac <tarika.common@gmail.com>
2020-02-12 23:30:40 -0600

    switched app backt o append

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ $.getJSON("/articles", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
-      $("#articles").prepend("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "<br />" + data[i].content + "</p>" );
+      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "<br />" + data[i].content + "</p>" );
     }
   });
   

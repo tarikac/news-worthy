b934f95ba599accf6353fd48b59999f7015b4801
tarikac <tarika.common@gmail.com>
2020-02-12 22:46:03 -0600

    removed gradient

--- public/app.js
+++ public/app.js
@@ -4,7 +4,6 @@ $.getJSON("/articles", function(data) {
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
       $("#articles").prepend("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "<br />" + data[i].content + "</p>" );
-    // Need to code for if there are so many entries, to delete the last 10 articles
     }
   });
   

00aa9d79c10edf396825603a05aa4d9033c40b44
tarikac <tarika.common@gmail.com>
2020-02-15 15:26:28 -0600

    now pulling from correct article class for content

--- server.js
+++ server.js
@@ -29,8 +29,7 @@ app.get("/scrape", function(req, res) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article").each(function(i, element) {
+    $("article.fp-vertical-story").each(function(i, element) {
       // Save an empty result object
       var result = {};
 

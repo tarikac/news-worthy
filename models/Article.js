6c55f1b16a6e2c4747b6a57d74b4c4522398c343
tarikac <tarika.common@gmail.com>
2020-02-15 00:25:51 -0600

    fixed article > content requirement to test heroku

--- models/Article.js
+++ models/Article.js
@@ -18,7 +18,7 @@ var ArticleSchema = new Schema({
   },
   content: {
       type: String,
-      required: true
+
   },
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
